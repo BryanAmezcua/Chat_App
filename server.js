c43818a20d3148e1b8fce5602d5099938ae2130d
Bryan Amezcua <bryanamezcua@gmail.com>
2020-06-04 23:44:27 -0700

    we can now send message. Time stamps are also accurate.

--- server.js
+++ server.js
@@ -12,7 +12,22 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 // Run when a client connects
 io.on('connection', socket => {
-    console.log('A user has connected');
+    // Welcome a user who has connected
+    socket.emit('message', 'Welcome to Chat App');
+
+    // Bradcast when a user connects - broadcast.emit will send a message to all users EXCEPT the user that has just connected
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // Run when client disconnects
+    socket.on('disconnect', () => {
+        // io.emit will send a message to ALL users that are connected
+        io.emit('message', 'A user has left the chat');
+    });
+
+    // listen for incoming chat message from client
+    socket.on('chat_message', chatMessage => {
+        io.emit('message', chatMessage);
+    });
 });
 
 const PORT = process.env.PORT || 3000;
