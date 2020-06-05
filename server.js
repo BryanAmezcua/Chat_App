13ec4e0a054dc758ee142a8c4634ea259380a0ff
Bryan Amezcua <bryanamezcua@gmail.com>
2020-06-04 21:21:39 -0700

    socket io is now working in the client. I was using app.listen when it should have been server.listen

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ io.on('connection', socket => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);});
+server.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);});
